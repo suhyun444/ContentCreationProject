1ce5a36c8d62da784403f635c98705f614395aef
정수현 <windseed71@naver.com>
2022-04-23 21:01:06 +0900

    애니메이션 시스템 완성

--- ContentCreationProject/AnimationState.cpp
+++ ContentCreationProject/AnimationState.cpp
@@ -1,1 +1,13 @@
 #include "AnimationState.h"
+AnimationState::AnimationState(std::string frameNames[], int frameRates[])
+{
+	int size = sizeof(frameRates) / sizeof(frameRates[0]);
+	for (int i = 0; i < size ; i++)
+	{
+		frames.push_back({ frameNames[i],frameRates[i] });
+	}
+}
+AnimationState::~AnimationState()
+{
+
+}
--- ContentCreationProject/AnimationState.h
+++ ContentCreationProject/AnimationState.h
@@ -1,5 +1,13 @@
 #pragma once
+#include <vector>
+#include <string>
 class AnimationState
 {
+public:
+	//spriteName / frameRate
+	AnimationState(std::string frames[],int frameRates[]);
+	~AnimationState();
+	std::vector<std::pair<std::string, int>> frames;
+	
 };
 
